07352fc30b087b325889796cdbb2f32113288777
Jakub Danicki <jakub.danicki@gmail.com>
2019-02-09 13:06:27 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { DatasourceComponent } from './datasource/datasource.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    DatasourceComponent
   ],
   imports: [
     BrowserModule,
